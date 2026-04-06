eb626ac68866ae88788e91526ddfabf283438f97
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-04-06 14:37:54 +0600

    feat: remove Subscribe module and related DTOs, update Payment and Webhook services

--- src/app/module/webhook/webhook.module.ts
+++ src/app/module/webhook/webhook.module.ts
@@ -4,17 +4,13 @@ import { WebhookController } from './webhook.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../user/entities/user.entity';
 import { Payment, PaymentSchema } from '../payment/entities/payment.entity';
-import {
-  Subscribe,
-  SubscribeSchema,
-} from '../subscribe/entities/subscribe.entity';
+
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: Payment.name, schema: PaymentSchema },
-      { name: Subscribe.name, schema: SubscribeSchema },
     ]),
   ],
   controllers: [WebhookController],
